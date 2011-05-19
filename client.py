jwyllie83/sshuttle

commit 2b68ea62d9989370a63bf3c1f30525c3337a8ca6

Support IPv4 and IPv6 at same time.

client.py
@@ -97,17 +97,67 @@ def original_dst(sock):
         raise
 
 
+class independent_listener:
+
+    def __init__(self, type=socket.SOCK_STREAM, proto=0):
+        self.v6 = socket.socket(socket.AF_INET6, type, proto)
+        self.v4 = socket.socket(socket.AF_INET, type, proto)
+
+    def setsockopt(self, level, optname, value):
+        if self.v6:
+            self.v6.setsockopt(level, optname, value)
+        if self.v4:
+            self.v4.setsockopt(level, optname, value)
+
+    def add_handler(self, handlers, handler):
+        if self.v6:
+            handlers.append(Handler([self.v6], lambda: handler(self.v6)))
+        if self.v4:
+            handlers.append(Handler([self.v4], lambda: handler(self.v4)))
+
+    def listen(self, backlog):
+        if self.v6:
+            self.v6.listen(backlog)
+        if self.v4:
+            try:
+                self.v4.listen(backlog)
+            except socket.error, e:
+                # on some systems v4 bind will fail if the v6 suceeded,
+                # in this case the v6 socket will receive v4 too.
+                if e.errno == errno.EADDRINUSE and self.v6:
+                    self.v4 = None
+                else:
+                    raise e
+
+    def bind(self, address_v4, address_v6):
+        if address_v6 and self.v6:
+            self.v6.bind(address_v6)
+        else:
+            self.v6 = None
+        if address_v4 and self.v4:
+            self.v4.bind(address_v4)
+        else:
+            self.v4 = None
+
+    def print_listening(self, what):
+        if self.v6:
+            listenip = self.v6.getsockname()
+            debug1('%s listening on %r.\n' % (what, listenip, ))
+        if self.v4:
+            listenip = self.v4.getsockname()
+            debug1('%s listening on %r.\n' % (what, listenip, ))
+
 class FirewallClient:
-    def __init__(self, port, subnets_include, subnets_exclude, dnsport, ipv6):
+    def __init__(self, port, subnets_include, subnets_exclude, dnsport, tproxy):
         self.port = port
         self.auto_nets = []
         self.subnets_include = subnets_include
         self.subnets_exclude = subnets_exclude
         self.dnsport = dnsport
-        self.ipv6 = ipv6
+        self.tproxy = tproxy
         argvbase = ([sys.argv[1], sys.argv[0], sys.argv[1]] +
                     ['-v'] * (helpers.verbose or 0) +
-                    ['--firewall', str(port), str(dnsport), str(ipv6 or '0')])
+                    ['--firewall', str(port), str(dnsport), str(tproxy or 0)])
         if ssyslog._p:
             argvbase += ['--syslog']
         argv_tries = [
@@ -154,10 +204,10 @@ class FirewallClient:
 
     def start(self):
         self.pfile.write('ROUTES\n')
-        for (ip,width) in self.subnets_include+self.auto_nets:
-            self.pfile.write('%d,0,%s\n' % (width, ip))
-        for (ip,width) in self.subnets_exclude:
-            self.pfile.write('%d,1,%s\n' % (width, ip))
+        for (family,ip,width) in self.subnets_include+self.auto_nets:
+            self.pfile.write('%d,%d,0,%s\n' % (family, width, ip))
+        for (family,ip,width) in self.subnets_exclude:
+            self.pfile.write('%d,%d,1,%s\n' % (family, width, ip))
         self.pfile.write('GO\n')
         self.pfile.flush()
         line = self.pfile.readline()
@@ -179,7 +229,7 @@ class FirewallClient:
 
 
 def _main(listener, fw, ssh_cmd, remotename, python, latency_control,
-          dnslistener, ipv6, seed_hosts, auto_nets,
+          dnslistener, tproxy, seed_hosts, auto_nets,
           syslog, daemon):
     handlers = []
     if helpers.verbose >= 1:
@@ -191,7 +241,7 @@ def _main(listener, fw, ssh_cmd, remotename, python, latency_control,
     try:
         (serverproc, serversock) = ssh.connect(ssh_cmd, remotename, python,
                         stderr=ssyslog._p and ssyslog._p.stdin,
-                        options=dict(latency_control=latency_control, ipv6=ipv6))
+                        options=dict(latency_control=latency_control, tproxy=tproxy))
     except socket.error, e:
         if e.args[0] == errno.EPIPE:
             raise Fatal("failed to establish ssh session (1)")
@@ -258,30 +308,30 @@ def _main(listener, fw, ssh_cmd, remotename, python, latency_control,
                 fw.sethostip(name, ip)
     mux.got_host_list = onhostlist
 
-    def onaccept():
+    def onaccept(listener_sock):
         global _extra_fd
         try:
-            sock,srcip = listener.accept()
+            sock,srcip = listener_sock.accept()
         except socket.error, e:
             if e.args[0] in [errno.EMFILE, errno.ENFILE]:
                 debug1('Rejected incoming connection: too many open files!\n')
                 # free up an fd so we can eat the connection
                 os.close(_extra_fd)
                 try:
-                    sock,srcip = listener.accept()
+                    sock,srcip = listener_sock.accept()
                     sock.close()
                 finally:
                     _extra_fd = os.open('/dev/null', os.O_RDONLY)
                 return
             else:
                 raise
-        if ipv6:
+        if tproxy:
             dstip = sock.getsockname();
         else:
             dstip = original_dst(sock)
         debug1('Accept: %s:%r -> %s:%r.\n' % (srcip[0],srcip[1],
                                               dstip[0],dstip[1]))
-        if dstip[1] == listener.getsockname()[1] and islocal(dstip[0]):
+        if dstip[1] == sock.getsockname()[1] and islocal(dstip[0]):
             debug1("-- ignored: that's my address!\n")
             sock.close()
             return
@@ -293,23 +343,23 @@ def _main(listener, fw, ssh_cmd, remotename, python, latency_control,
         mux.send(chan, ssnet.CMD_CONNECT, '%s,%r' % (dstip[0], dstip[1]))
         outwrap = MuxWrapper(mux, chan)
         handlers.append(Proxy(SockWrapper(sock, sock), outwrap))
-    handlers.append(Handler([listener], onaccept))
+    listener.add_handler(handlers, onaccept)
 
     dnsreqs = {}
     def dns_done(chan, data):
-        peer,timeout = dnsreqs.get(chan) or (None,None)
+        peer,sock,timeout = dnsreqs.get(chan) or (None,None)
         debug3('dns_done: channel=%r peer=%r\n' % (chan, peer))
         if peer:
             del dnsreqs[chan]
             debug3('doing sendto %r\n' % (peer,))
-            dnslistener.sendto(data, peer)
-    def ondns():
-        pkt,peer = dnslistener.recvfrom(4096)
+            sock.sendto(data, peer)
+    def ondns(listener_sock):
+        pkt,peer = listener_sock.recvfrom(4096)
         now = time.time()
         if pkt:
             debug1('DNS request from %r: %d bytes\n' % (peer, len(pkt)))
             chan = mux.next_channel()
-            dnsreqs[chan] = peer,now+30
+            dnsreqs[chan] = peer,listener_sock,now+30
             mux.send(chan, ssnet.CMD_DNS_REQ, pkt)
             mux.channels[chan] = lambda cmd,data: dns_done(chan,data)
         for chan,(peer,timeout) in dnsreqs.items():
@@ -317,7 +367,7 @@ def _main(listener, fw, ssh_cmd, remotename, python, latency_control,
                 del dnsreqs[chan]
         debug3('Remaining DNS requests: %d\n' % len(dnsreqs))
     if dnslistener:
-        handlers.append(Handler([dnslistener], ondns))
+        dnslistener.add_handler(handlers, ondsn)
 
     if seed_hosts != None:
         debug1('seed_hosts: %r\n' % seed_hosts)
@@ -334,8 +384,9 @@ def _main(listener, fw, ssh_cmd, remotename, python, latency_control,
         mux.callback()
 
 
-def main(listenip, ssh_cmd, remotename, python, latency_control, dns,
-         ipv6, seed_hosts, auto_nets,
+def main(listenip_v6, listenip_v4,
+         ssh_cmd, remotename, python, latency_control, dns,
+         tproxy, seed_hosts, auto_nets,
          subnets_include, subnets_exclude, syslog, daemon, pidfile):
     if syslog:
         ssyslog.start_syslog()
@@ -347,58 +398,70 @@ def main(listenip, ssh_cmd, remotename, python, latency_control, dns,
             return 5
     debug1('Starting sshuttle proxy.\n')
     
-    if listenip[1]:
+    if listenip_v4[1]:
         ports = [listenip[1]]
     else:
         ports = xrange(12300,9000,-1)
     last_e = None
+    redirectport = None
     bound = False
     debug2('Binding:')
     for port in ports:
         debug2(' %d' % port)
-        if ipv6:
-            listener = socket.socket(socket.AF_INET6)
-        else:
-            listener = socket.socket(socket.AF_INET)
+        listener = independent_listener()
         listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        try:
+
+        if tproxy:
             listener.setsockopt(socket.SOL_IP, IP_TRANSPARENT, 1)
-        except socket.error, e:
-            last_e = e
-        if ipv6:
-            dnslistener = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-        else:
-            dnslistener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        dnslistener = independent_listener(socket.SOCK_DGRAM)
         dnslistener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         try:
-            listener.bind((listenip[0], port))
-            dnslistener.bind((listenip[0], port))
+            if listenip_v6:
+                lv6 = (listenip_v6[0],port)
+            else:
+                lv6 = None
+            if listenip_v4:
+                lv4 = (listenip_v4[0],port)
+            else:
+                lv4 = None
+            listener.bind(lv4, lv6)
+            dnslistener.bind(lv4, lv6)
+            redirectport = port
             bound = True
             break
+        except Fatal, e:
+            raise e
         except socket.error, e:
-            last_e = e
+            if e.errno == errno.EADDRNOTAVAIL:
+                last_e = e
+            else:
+                raise e
     debug2('\n')
     if not bound:
         assert(last_e)
         raise last_e
     listener.listen(10)
-    listenip = listener.getsockname()
-    debug1('Listening on %r.\n' % (listenip,))
+    listener.print_listening("Redirector")
 
     if dns:
-        dnsip = dnslistener.getsockname()
-        debug1('DNS listening on %r.\n' % (dnsip,))
-        dnsport = dnsip[1]
+        dnslistener.print_listening("DNS")
+        if dnslistener.v6:
+            dnsip = dnslistener.v6.getsockname()
+            dnsport = dnsip[1]
+        else:
+            dnsip = dnslistener.v4.getsockname()
+            dnsport = dnsip[1]
     else:
         dnsport = 0
         dnslistener = None
 
-    fw = FirewallClient(listenip[1], subnets_include, subnets_exclude, dnsport, ipv6)
+    fw = FirewallClient(redirectport, subnets_include, subnets_exclude, dnsport, tproxy)
     
     try:
         return _main(listener, fw, ssh_cmd, remotename,
                      python, latency_control, dnslistener,
-                     ipv6, seed_hosts, auto_nets, syslog, 
+                     tproxy, seed_hosts, auto_nets, syslog, 
                      daemon)
     finally:
         try:

firewall.py
@@ -14,13 +14,15 @@ def nonfatal(func, *args):
         log('error: %s\n' % e)
 
 
-def ipt_chain_exists(name, ipv6):
-    if ipv6:
+def ipt_chain_exists(name, family):
+    if family == socket.AF_INET6:
         table = 'mangle'
         cmd = 'ip6tables'
-    else:
+    elif family == socket.AF_INET:
         table = 'nat'
         cmd = 'iptables'
+    else:
+        raise Fatal("Unsupported socket type '%s'"%family)
     argv = [cmd, '-t', table, '-nL']
     p = ssubprocess.Popen(argv, stdout = ssubprocess.PIPE)
     for line in p.stdout:
@@ -31,7 +33,7 @@ def ipt_chain_exists(name, ipv6):
         raise Fatal('%r returned %d' % (argv, rv))
 
 
-def ipt(*args):
+def ipt4(*args):
     argv = ['iptables', '-t', 'nat'] + list(args)
     debug1('>> %s\n' % ' '.join(argv))
     rv = ssubprocess.call(argv)
@@ -45,9 +47,17 @@ def ipt6(*args):
     if rv:
         raise Fatal('%r returned %d' % (argv, rv))
 
+def ipt(family, *args):
+    if family == socket.AF_INET6:
+        ipt6(*args)
+    elif family == socket.AF_INET:
+        ipt4(*args)
+    else:
+        raise Fatal("Unsupported socket type '%s'"%family)
+    
 
 _no_ttl_module = False
-def ipt_ttl(*args):
+def ipt_ttl(family, *args):
     global _no_ttl_module
     if not _no_ttl_module:
         # we avoid infinite loops by generating server-side connections
@@ -55,15 +65,15 @@ def ipt_ttl(*args):
         # connections, in case client == server.
         try:
             argsplus = list(args) + ['-m', 'ttl', '!', '--ttl', '42']
-            ipt(*argsplus)
+            ipt(family, *argsplus)
         except Fatal:
-            ipt(*args)
+            ipt(family, *args)
             # we only get here if the non-ttl attempt succeeds
             log('sshuttle: warning: your iptables is missing '
                 'the ttl module.\n')
             _no_ttl_module = True
     else:
-        ipt(*args)
+        ipt(family, *args)
 
 
 
@@ -72,21 +82,25 @@ def ipt_ttl(*args):
 # multiple copies shouldn't have overlapping subnets, or only the most-
 # recently-started one will win (because we use "-I OUTPUT 1" instead of
 # "-A OUTPUT").
-def do_iptables(port, dnsport, ipv6, subnets):
+def do_iptables_nat(port, dnsport, family, subnets):
+    # only ipv4 supported with NAT
+    if family != socket.AF_INET:
+        return
+
     chain = 'sshuttle-%s' % port
 
     # basic cleanup/setup of chains
-    if ipt_chain_exists(chain, ipv6):
-        nonfatal(ipt, '-D', 'OUTPUT', '-j', chain)
-        nonfatal(ipt, '-D', 'PREROUTING', '-j', chain)
-        nonfatal(ipt, '-F', chain)
-        ipt('-X', chain)
+    if ipt_chain_exists(chain, family):
+        nonfatal(ipt, family, '-D', 'OUTPUT', '-j', chain)
+        nonfatal(ipt, family, '-D', 'PREROUTING', '-j', chain)
+        nonfatal(ipt, family, '-F', chain)
+        ipt(family, '-X', chain)
 
     if subnets or dnsport:
-        ipt('-N', chain)
-        ipt('-F', chain)
-        ipt('-I', 'OUTPUT', '1', '-j', chain)
-        ipt('-I', 'PREROUTING', '1', '-j', chain)
+        ipt(family, '-N', chain)
+        ipt(family, '-F', chain)
+        ipt(family, '-I', 'OUTPUT', '1', '-j', chain)
+        ipt(family, '-I', 'PREROUTING', '1', '-j', chain)
 
     if subnets:
         # create new subnet entries.  Note that we're sorting in a very
@@ -94,13 +108,15 @@ def do_iptables(port, dnsport, ipv6, subnets):
         # to least-specific, and at any given level of specificity, we want
         # excludes to come first.  That's why the columns are in such a non-
         # intuitive order.
-        for swidth,sexclude,snet in sorted(subnets, reverse=True):
-            if sexclude:
-                ipt('-A', chain, '-j', 'RETURN',
+        for f,swidth,sexclude,snet in sorted(subnets, reverse=True):
+            if f != family:
+                pass
+            elif sexclude:
+                ipt(family, '-A', chain, '-j', 'RETURN',
                     '--dest', '%s/%s' % (snet,swidth),
                     '-p', 'tcp')
             else:
-                ipt_ttl('-A', chain, '-j', 'REDIRECT',
+                ipt_ttl(family, '-A', chain, '-j', 'REDIRECT',
                         '--dest', '%s/%s' % (snet,swidth),
                         '-p', 'tcp',
                         '--to-ports', str(port))
@@ -108,53 +124,58 @@ def do_iptables(port, dnsport, ipv6, subnets):
     if dnsport:
         nslist = resolvconf_nameservers()
         for ip in nslist:
-            ipt_ttl('-A', chain, '-j', 'REDIRECT',
+            ipt_ttl(family, '-A', chain, '-j', 'REDIRECT',
                     '--dest', '%s/32' % ip,
                     '-p', 'udp',
                     '--dport', '53',
                     '--to-ports', str(dnsport))
 
-def do_ip6tables(port, dnsport, ipv6, subnets):
+def do_ip6tables_tproxy(port, dnsport, family, subnets):
+    if family not in [socket.AF_INET, socket.AF_INET6]:
+        return
+
     mark_chain   = 'sshuttle-m-%s' % port
     tproxy_chain = 'sshuttle-t-%s' % port
 
     # basic cleanup/setup of chains
-    if ipt_chain_exists(mark_chain, ipv6):
-        ipt6('-D', 'OUTPUT', '-j', mark_chain)
-        ipt6('-F', mark_chain)
-        ipt6('-X', mark_chain)
+    if ipt_chain_exists(mark_chain, family):
+        ipt(family, '-D', 'OUTPUT', '-j', mark_chain)
+        ipt(family, '-F', mark_chain)
+        ipt(family, '-X', mark_chain)
 
-    if ipt_chain_exists(tproxy_chain, ipv6):
-        ipt6('-D', 'PREROUTING', '-j', tproxy_chain)
-        ipt6('-F', tproxy_chain)
-        ipt6('-X', tproxy_chain)
+    if ipt_chain_exists(tproxy_chain, family):
+        ipt(family, '-D', 'PREROUTING', '-j', tproxy_chain)
+        ipt(family, '-F', tproxy_chain)
+        ipt(family, '-X', tproxy_chain)
 
     if subnets or dnsport:
-        ipt6('-N', mark_chain)
-        ipt6('-F', mark_chain)
-        ipt6('-N', tproxy_chain)
-        ipt6('-F', tproxy_chain)
-        ipt6('-I', 'OUTPUT', '1', '-j', mark_chain)
-        ipt6('-I', 'PREROUTING', '1', '-j', tproxy_chain)
-        ipt6('-A', tproxy_chain, '-m', 'socket', '-j', 'RETURN',
+        ipt(family, '-N', mark_chain)
+        ipt(family, '-F', mark_chain)
+        ipt(family, '-N', tproxy_chain)
+        ipt(family, '-F', tproxy_chain)
+        ipt(family, '-I', 'OUTPUT', '1', '-j', mark_chain)
+        ipt(family, '-I', 'PREROUTING', '1', '-j', tproxy_chain)
+        ipt(family, '-A', tproxy_chain, '-m', 'socket', '-j', 'RETURN',
              '-m', 'tcp', '-p', 'tcp')
 
-    #ipt6('-A', mark_chain, '-o', 'lo', '-j', 'RETURN')
+    #ipt(family, '-A', mark_chain, '-o', 'lo', '-j', 'RETURN')
  
     if subnets:
-        for swidth,sexclude,snet in sorted(subnets, reverse=True):
-            if sexclude:
-                ipt6('-A', mark_chain, '-j', 'RETURN',
+        for f,swidth,sexclude,snet in sorted(subnets, reverse=True):
+            if f != family:
+                pass
+            elif sexclude:
+                ipt(family, '-A', mark_chain, '-j', 'RETURN',
                     '--dest', '%s/%s' % (snet,swidth),
                     '-m', 'tcp', '-p', 'tcp')
-                ipt6('-A', tproxy_chain, '-j', 'RETURN',
+                ipt(family, '-A', tproxy_chain, '-j', 'RETURN',
                     '--dest', '%s/%s' % (snet,swidth),
                     '-m', 'tcp', '-p', 'tcp')
             else:
-                ipt6('-A', mark_chain, '-j', 'MARK', '--set-mark', '1',
+                ipt(family, '-A', mark_chain, '-j', 'MARK', '--set-mark', '1',
                      '--dest', '%s/%s' % (snet,swidth),
                      '-m', 'tcp', '-p', 'tcp')
-                ipt6('-A', tproxy_chain, '-j', 'TPROXY', '--tproxy-mark', '0x1/0x1',
+                ipt(family, '-A', tproxy_chain, '-j', 'TPROXY', '--tproxy-mark', '0x1/0x1',
                      '--dest', '%s/%s' % (snet,swidth),
                      '-m', 'tcp', '-p', 'tcp',
                      '--on-port', str(port))
@@ -265,7 +286,11 @@ def ipfw(*args):
         raise Fatal('%r returned %d' % (argv, rv))
 
 
-def do_ipfw(port, dnsport, ipv6, subnets):
+def do_ipfw(port, dnsport, family, subnets):
+    # IPv6 support TODO
+    if family != socket.AF_INET:
+        return
+
     sport = str(port)
     xsport = str(port+1)
 
@@ -427,7 +452,7 @@ def restore_etc_hosts(port):
 # exit.  In case that fails, it's not the end of the world; future runs will
 # supercede it in the transproxy list, at least, so the leftover rules
 # are hopefully harmless.
-def main(port, dnsport, ipv6, syslog):
+def main(port, dnsport, tproxy, syslog):
     assert(port > 0)
     assert(port <= 65535)
     assert(dnsport >= 0)
@@ -439,10 +464,10 @@ def main(port, dnsport, ipv6, syslog):
     if program_exists('ipfw'):
         do_it = do_ipfw
     elif program_exists('iptables'):
-        if ipv6:
-            do_it = do_ip6tables
+        if tproxy:
+            do_it = do_ip6tables_tproxy
         else:
-            do_it = do_iptables
+            do_it = do_iptables_nat
     else:
         raise Fatal("can't find either ipfw or iptables; check your PATH")
 
@@ -480,15 +505,16 @@ def main(port, dnsport, ipv6, syslog):
         elif line == 'GO\n':
             break
         try:
-            (width,exclude,ip) = line.strip().split(',', 2)
+            (family,width,exclude,ip) = line.strip().split(',', 3)
         except:
             raise Fatal('firewall: expected route or GO but got %r' % line)
-        subnets.append((int(width), bool(int(exclude)), ip))
+        subnets.append((int(family), int(width), bool(int(exclude)), ip))
         
     try:
         if line:
             debug1('firewall manager: starting transproxy.\n')
-            do_wait = do_it(port, dnsport, ipv6, subnets)
+            do_wait = do_it(port, dnsport, socket.AF_INET6, subnets)
+            do_wait = do_it(port, dnsport, socket.AF_INET, subnets)
             sys.stdout.write('STARTED\n')
         
         try:
@@ -517,5 +543,6 @@ def main(port, dnsport, ipv6, syslog):
             debug1('firewall manager: undoing changes.\n')
         except:
             pass
-        do_it(port, 0, ipv6, [])
+        do_it(port, 0, socket.AF_INET6, [])
+        do_it(port, 0, socket.AF_INET, [])
         restore_etc_hosts(port)

helpers.py
@@ -1,4 +1,4 @@
-import sys, os, socket
+import sys, os, socket, errno
 
 logprefix = ''
 verbose = 0

main.py
@@ -1,50 +1,53 @@
-import sys, os, re
+import sys, os, re, socket
 import helpers, options, client, server, firewall, hostwatch
 import compat.ssubprocess as ssubprocess
 from helpers import *
 
 
+def parse_subnet4(s):
+    m = re.match(r'(\d+)(?:\.(\d+)\.(\d+)\.(\d+))?(?:/(\d+))?$', s)
+    if not m:
+        raise Fatal('%r is not a valid IP subnet format' % s)
+    (a,b,c,d,width) = m.groups()
+    (a,b,c,d) = (int(a or 0), int(b or 0), int(c or 0), int(d or 0))
+    if width == None:
+        width = 32
+    else:
+        width = int(width)
+    if a > 255 or b > 255 or c > 255 or d > 255:
+        raise Fatal('%d.%d.%d.%d has numbers > 255' % (a,b,c,d))
+    if width > 32:
+        raise Fatal('*/%d is greater than the maximum of 32' % width)
+    return(socket.AF_INET, '%d.%d.%d.%d' % (a,b,c,d), width)
+
+def parse_subnet6(s):
+    m = re.match(r'(?:([a-fA-F\d:]+))?(?:/(\d+))?$', s)
+    if not m:
+        raise Fatal('%r is not a valid IP subnet format' % s)
+    (net,width) = m.groups()
+    if width == None:
+        width = 128
+    else:
+        width = int(width)
+    if width > 128:
+        raise Fatal('*/%d is greater than the maximum of 128' % width)
+    return(socket.AF_INET6, net, width)
+
 # list of:
 # 1.2.3.4/5 or just 1.2.3.4
+# 1:2::3/64 or just 1:2::3
 def parse_subnets(subnets_str):
     subnets = []
     for s in subnets_str:
-        m = re.match(r'(\d+)(?:\.(\d+)\.(\d+)\.(\d+))?(?:/(\d+))?$', s)
-        if not m:
-            raise Fatal('%r is not a valid IP subnet format' % s)
-        (a,b,c,d,width) = m.groups()
-        (a,b,c,d) = (int(a or 0), int(b or 0), int(c or 0), int(d or 0))
-        if width == None:
-            width = 32
+        if ':' in s:
+            subnet = parse_subnet6(s)
         else:
-            width = int(width)
-        if a > 255 or b > 255 or c > 255 or d > 255:
-            raise Fatal('%d.%d.%d.%d has numbers > 255' % (a,b,c,d))
-        if width > 32:
-            raise Fatal('*/%d is greater than the maximum of 32' % width)
-        subnets.append(('%d.%d.%d.%d' % (a,b,c,d), width))
-    return subnets
-
-# list of:
-# 1:2::3/64 or just 1:2::3
-def parse_subnets6(subnets_str):
-    subnets = []
-    for s in subnets_str:
-        m = re.match(r'(?:([a-fA-F\d:]+))?(?:/(\d+))?$', s)
-        if not m:
-            raise Fatal('%r is not a valid IP subnet format' % s)
-        (net,width) = m.groups()
-        if width == None:
-            width = 128
-        else:
-            width = int(width)
-        if width > 128:
-            raise Fatal('*/%d is greater than the maximum of 128' % width)
-        subnets.append((net, width))
+            subnet = parse_subnet4(s)
+        subnets.append(subnet)
     return subnets
 
 # 1.2.3.4:567 or just 1.2.3.4 or just 567
-def parse_ipport(s):
+def parse_ipport4(s):
     s = str(s)
     m = re.match(r'(?:(\d+)\.(\d+)\.(\d+)\.(\d+))?(?::)?(?:(\d+))?$', s)
     if not m:
@@ -81,7 +84,7 @@ l,listen=  transproxy to this ip address and port number
 H,auto-hosts scan for remote hostnames and update local /etc/hosts
 N,auto-nets  automatically determine subnets to route
 dns        capture local DNS requests and forward to the remote DNS server
-6,ipv6     mode for dealing with IPv6
+tproxy     tproxy support
 python=    path to python interpreter on the remote server
 r,remote=  ssh hostname (and optional username) of remote sshuttle server
 x,exclude= exclude this subnet (can be used more than once)
@@ -112,7 +115,7 @@ try:
         if len(extra) != 0:
             o.fatal('no arguments expected')
         server.latency_control = opt.latency_control
-        server.ipv6 = opt.ipv6
+        server.tproxy = opt.tproxy
         sys.exit(server.main())
     elif opt.firewall:
         if len(extra) != 3:
@@ -125,10 +128,7 @@ try:
         if len(extra) < 1 and not opt.auto_nets:
             o.fatal('at least one subnet (or -N) expected')
         includes = extra
-        if not opt.ipv6:
-            excludes = ['127.0.0.0/8']
-        else:
-            excludes = [] #FIXME
+        excludes = ['127.0.0.0/8']
         for k,v in flags:
             if k in ('-x','--exclude'):
                 excludes.append(v)
@@ -143,23 +143,26 @@ try:
             sh = []
         else:
             sh = None
-        if opt.ipv6:
-            ipport = parse_ipport6(opt.listen or '[::]:0')
-            do_parse_subnets = parse_subnets6
+        if not opt.listen:
+            ipport_v6 = parse_ipport6('[::]:0')
+            ipport_v4 = parse_ipport4('127.0.0.1:0')
+        elif ':' in opt.listen:
+            ipport_v6 = parse_ipport6(opt.listen or '[::]:0')
+            ipport_v4 = None
         else:
-            ipport = parse_ipport(opt.listen or '127.0.0.1:0')
-            do_parse_subnets = parse_subnets
-        sys.exit(client.main(ipport,
+            ipport_v6 = None
+            ipport_v4 = parse_ipport4(opt.listen or '127.0.0.1:0')
+        sys.exit(client.main(ipport_v6, ipport_v4,
                              opt.ssh_cmd,
                              remotename,
                              opt.python,
                              opt.latency_control,
                              opt.dns,
-                             opt.ipv6,
+                             opt.tproxy,
                              sh,
                              opt.auto_nets,
-                             do_parse_subnets(includes),
-                             do_parse_subnets(excludes),
+                             parse_subnets(includes),
+                             parse_subnets(excludes),
                              opt.syslog, opt.daemon, opt.pidfile))
 except Fatal, e:
     log('fatal: %s\n' % e)

server.py
@@ -166,7 +166,6 @@ def main():
     else:
         helpers.logprefix = 'server: '
     debug1('latency control setting = %r\n' % latency_control)
-    debug1('IPv6 mode = %r\n' % ipv6)
     routes = list(list_routes())
     debug1('available routes:\n')
     for r in routes:
@@ -215,7 +214,7 @@ def main():
     def new_channel(channel, data):
         (dstip,dstport) = data.split(',', 1)
         dstport = int(dstport)
-        outwrap = ssnet.connect_dst(dstip,dstport,ipv6)
+        outwrap = ssnet.connect_dst(dstip,dstport)
         handlers.append(Proxy(MuxWrapper(mux, channel), outwrap))
     mux.new_channel = new_channel
 

ssh.py
@@ -27,9 +27,7 @@ def connect(ssh_cmd, rhostport, python, stderr, options):
     main_exe = sys.argv[0]
     portl = []
 
-    rhostIsIPv6 = False
     if (rhostport or '').count(':') > 1:
-        rhostIsIPv6 = True
         if rhostport.count(']') or rhostport.count('['):
             result = rhostport.split(']')
             rhost = result[0].strip('[')
@@ -47,10 +45,6 @@ def connect(ssh_cmd, rhostport, python, stderr, options):
 
     if rhost == '-':
         rhost = None
-
-    ipv6flag = []
-    if rhostIsIPv6:
-        ipv6flag = ['-6']
 
     z = zlib.compressobj(1)
     content = readfile('assembler.py')
@@ -88,7 +82,6 @@ def connect(ssh_cmd, rhostport, python, stderr, options):
                      "exec \"$P\" -c '%s'") % pyscript
         argv = (sshl + 
                 portl + 
-                ipv6flag + 
                 [rhost, '--', pycmd])
     (s1,s2) = socket.socketpair()
     def setup():

ssnet.py
@@ -511,12 +511,13 @@ class MuxWrapper(SockWrapper):
                             % (cmd, len(data)))
 
 
-def connect_dst(ip, port, ipv6):
+def connect_dst(ip, port):
     debug2('Connecting to %s:%d\n' % (ip, port))
-    if ipv6:
-        outsock = socket.socket(socket.AF_INET6)
+    if ':' in ip:
+        family = socket.AF_INET6
     else:
-        outsock = socket.socket(socket.AF_INET)
+        family = socket.AF_INET
+    outsock = socket.socket(family)
     outsock.setsockopt(socket.SOL_IP, socket.IP_TTL, 42)
     return SockWrapper(outsock, outsock,
                        connect_to = (ip,port),

commit 10247a0147cf57ff2c6fbfd3118c8d5d0e8b3ef5

Fix stupid typo introduced by 2b68ea62d9989370a63bf3c1f30525c3337a8ca6.

client.py
@@ -367,7 +367,7 @@ def _main(listener, fw, ssh_cmd, remotename, python, latency_control,
                 del dnsreqs[chan]
         debug3('Remaining DNS requests: %d\n' % len(dnsreqs))
     if dnslistener:
-        dnslistener.add_handler(handlers, ondsn)
+        dnslistener.add_handler(handlers, ondns)
 
     if seed_hosts != None:
         debug1('seed_hosts: %r\n' % seed_hosts)