134eb5fcd30c5658a5eaf178e7b9304a9063c0c5
Jim Wyllie <jwyllie83@gmail.com>
2016-09-18 16:19:42 -0400

    Tested and working!
    My local setup requires a work-ssh tunnel-within-a-tunnel, but that's in a
    separate repo.

--- packaging/sshuttle-post-stop.sh
+++ packaging/sshuttle-post-stop.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+# Log what's going on
+# Intuitively, I'm thinking this needs some kind of decorator, where I can
+# write a Bash function like echo that will redirect to some static location.
+# I can't think of an obvious way to do that for like three commands that
+# doesn't involve shell expansion hell, so I'm not going to bother.
+export LOGFILE=/tmp/sshuttle/sshuttle-post-stop.sh.logs
+mkdir -p "$(dirname "${LOGFILE}")"
+
+: > "${LOGFILE}"
+
 if [ -f "${PREFIX_LOCATION}" ]; then
 	cat "${PREFIX_LOCATION}" | while read ROUTE; do
 
@@ -12,13 +23,13 @@ if [ -f "${PREFIX_LOCATION}" ]; then
 		fi
 
 		echo "Deleting route: ${ROUTE}"
-		ip route del local ${ROUTE} dev lo table ${ROUTE_TABLE}
+		ip route del local ${ROUTE} dev lo table ${ROUTE_TABLE} >> "${LOGFILE}" 2>&1
 	done
 fi
 
-ip rule del fwmark ${FWMARK}
+ip rule del fwmark ${FWMARK} >> "${LOGFILE}" 2>&1
 
 for RUNFILE in ${MISC_STOP_DIR}/*; do
 	echo "Executing ${RUNFILE}"
-	/bin/sh -c "${RUNFILE}"
+	/bin/sh -c "${RUNFILE}" >> "${LOGFILE}" 2>&1
 done
--- packaging/sshuttle-pre-start.sh
+++ packaging/sshuttle-pre-start.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+# Log what's going on
+# Intuitively, I'm thinking this needs some kind of decorator, where I can
+# write a Bash function like echo that will redirect to some static location.
+# I can't think of an obvious way to do that for like three commands that
+# doesn't involve shell expansion hell, so I'm not going to bother.
+export LOGFILE=/tmp/sshuttle/sshuttle-pre-start.sh.logs
+mkdir -p "$(dirname "${LOGFILE}")"
+
+: > "${LOGFILE}"
+
 # Make sure we have created the routes
-ip rule add fwmark ${FWMARK} lookup ${ROUTE_TABLE}
-echo "Starting sshuttle..."
+ip rule add fwmark ${FWMARK} lookup ${ROUTE_TABLE} >> "${LOGFILE}" 2>&1
+echo "Starting sshuttle..." >> "${LOGFILE}" 2>&1
 
 if [ -f "${PREFIX_LOCATION}" ]; then
 	cat "${PREFIX_LOCATION}" | while read ROUTE; do
@@ -15,13 +26,12 @@ if [ -f "${PREFIX_LOCATION}" ]; then
 			continue
 		fi
 
-		echo "Adding route: ${ROUTE}"
-		ip route add local ${ROUTE} dev lo table ${ROUTE_TABLE}
+		echo "Adding route: ${ROUTE}" >> "${LOGFILE}" 2>&1
+		ip route add local ${ROUTE} dev lo table ${ROUTE_TABLE} >> "${LOGFILE}" 2>&1
 	done
 fi
 
 for RUNFILE in ${MISC_START_DIR}/*; do
-	echo "Executing ${RUNFILE}"
-	/bin/sh -c "${RUNFILE}" &
-	sleep 1.5
+	echo "Executing ${RUNFILE}" >> "${LOGFILE}" 2>&1
+	/bin/sh -c "${RUNFILE}" >> "${LOGFILE}" 2>&1
 done
